fd28fdc521e3efb0ed71575c1c4a28354c9b4b5a
Ned Ruggeri <ruggeri@self-loop.com>
2014-01-16 16:52:33 -0800

    checkin api layer

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,20 @@
+class Api::CommentsController < ApplicationController
+  def create
+    @comment = TodoComment.new(params[:comment])
+    if @comment.save
+      render :json => @comment
+    else
+      render :json => @comment.errors, :status => :unprocessable_entity
+    end
+  end
+
+  def index
+    @comments = TodoComment.where(:todo_id => params[:todo_id])
+    render :json => @comments
+  end
+  
+  def show
+    @comment = TodoComment.find(params[:id])
+    render :json => @comment
+  end
+end
---
+++ app/controllers/api/todos_controller.rb
@@ -0,0 +1,21 @@
+class Api::TodosController < ApplicationController
+  def create
+    @todo = Todo.new(params[:todo])
+
+    if @todo.save
+      render :json => @todo
+    else
+      render :json => @todo.errors, :status => :unprocessable_entity
+    end
+  end
+  
+  def index
+    @todos = Todo.all
+    render :json => @todos
+  end
+  
+  def show
+    @todo = Todo.find(params[:id])
+    render :json => @todo
+  end
+end
