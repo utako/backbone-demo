c789827c228391aa6e6866a1f2e215c1afcf2203
Ned Ruggeri <ruggeri@self-loop.com>
2014-01-18 15:36:36 -0800

    add comment deletion

--- app/assets/javascripts/views/todos_show.js
+++ app/assets/javascripts/views/todos_show.js
@@ -3,18 +3,25 @@ window.Todo.Views.TodosShow = Backbone.View.extend({
 
   initialize: function (options) {
     this.listenTo(this.model, "sync", this.render);
-    this.listenTo(this.model.comments(), "sync add", this.render);
+    this.listenTo(this.model.comments(), "sync add remove", this.render);
   },
 
   render: function () {
     var renderedContent = this.template({
-      todo: this.model,
-      // comments: this.comments
+      todo: this.model
     });
 
     this.$el.html(renderedContent);
 
-    // TODO: don't like rebuilding new view on every render...
+    // even more building view objects inside #render...
+    this.model.comments().each(function (comment) {
+      var commentsShowView = new Todo.Views.CommentsShow({
+        model: comment
+      });
+
+      this.$(".comments").append(commentsShowView.render().$el);
+    });
+
     var commentNewView = new Todo.Views.CommentsNew({
       todo: this.model
     });
