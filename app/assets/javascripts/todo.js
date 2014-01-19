1f7a718e2b3c2c101d2411cc9e7f16eb450e3da3
Ned Ruggeri <ruggeri@self-loop.com>
2014-01-19 10:56:28 -0800

    restore rendering of comment show views

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -13,11 +13,14 @@ window.Todo = {
 };
 
 Backbone.CompositeView = Backbone.View.extend({
-  addSubview: function (selector, view) {
+  addSubview: function (selector, subview) {
     var selectorSubviews =
       this.subviews()[selector] || (this.subviews()[selector] = []);
 
-    selectorSubviews.push(view);
+    selectorSubviews.push(subview);
+
+    var $selectorEl = this.$(selector);
+    $selectorEl.append(subview.$el);
   },
 
   renderSubviews: function () {
--- app/assets/javascripts/views/todos_show.js
+++ app/assets/javascripts/views/todos_show.js
@@ -3,22 +3,21 @@ window.Todo.Views.TodosShow = Backbone.CompositeView.extend({
 
   initialize: function (options) {
     this.listenTo(this.model, "sync", this.render);
-    this.listenTo(this.model.comments(), "sync add remove", this.render);
-
-    // // even more building view objects inside #render...
-    // this.model.comments().each(function (comment) {
-    //   var commentsShowView = new Todo.Views.CommentsShow({
-    //     model: comment
-    //   });
-    // 
-    //   this.addSubview(".comments", commentsShowView);
-    //   // this.$(".comments").append(commentsShowView.render().$el);
-    // });
+    this.listenTo(this.model.comments(), "add", this.addComment);
 
     var commentNewView = new Todo.Views.CommentsNew({
       todo: this.model
     });
     this.addSubview(".comment-new", commentNewView);
+  },
+
+  addComment: function (comment) {
+    var commentsShowView = new Todo.Views.CommentsShow({
+      model: comment
+    });
+    
+    this.addSubview(".comments", commentsShowView);
+    commentsShowView.render();
   },
 
   render: function () {
